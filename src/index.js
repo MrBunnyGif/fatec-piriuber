83a853c3508c536107b1d85b8cad1a5ad332d7cf
Note Atelie <pedro.botega@ateliedepropaganda.com.br>
2022-06-01 12:39:19 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+
+ReactDOM.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+    </Routes>
+  </BrowserRouter>,
+  document.getElementById('root')
+);
