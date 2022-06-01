4ab465bdf60a39859f48dfbb335f3409e02e7b19
Note Atelie <pedro.botega@ateliedepropaganda.com.br>
2022-06-01 12:55:26 -0300

    Creates routes

--- src/index.js
+++ src/index.js
@@ -8,12 +8,31 @@ import {
   Route,
 } from "react-router-dom";
 import LoginType from './pages/public/Logintype'
+import DriverContact from './pages/private/Parents/DriverContact'
+import DriversList from './pages/private/Parents/DriversList'
+import FindChildren from './pages/private/Parents/FindChildren'
+import ParentsProfile from './pages/private/Parents/Profile'
+import CarRoutes from './pages/private/Driver/CarRoutes'
+import Checkin from './pages/private/Driver/Checkin'
+import Children from './pages/private/Driver/Children'
+import ParentsContact from './pages/private/Driver/ParentsContact'
+import Home from './pages/private/Home'
 
 ReactDOM.render(
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/login-type" element={<LoginType />} />
+      <Route path="/" element={<LoginType />} />
+      <Route path="/home" element={<Home />} />
+
+      <Route path="/driver-contact" element={<DriverContact />} />
+      <Route path="/drivers-list" element={<DriversList />} />
+      <Route path="/find-children" element={<FindChildren />} />
+      <Route path="/parents-profile" element={<ParentsProfile />} />
+
+      <Route path="/car-routes" element={<CarRoutes />} />
+      <Route path="/checkin" element={<Checkin />} />
+      <Route path="/manage-children" element={<Children />} />
+      <Route path="/parents-contact" element={<ParentsContact />} />
     </Routes>
   </BrowserRouter>,
   document.getElementById('root')
--- src/pages/private/Parents/DriversContact.js
+++
@@ -1,2 +0,0 @@
-import React from 'react'
-export default () => <h1>Drivers contact</h1>
