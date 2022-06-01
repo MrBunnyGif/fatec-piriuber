953c037ee8e251f9adcb476864d0587442de9e09
Note Atelie <pedro.botega@ateliedepropaganda.com.br>
2022-06-01 13:12:45 -0300

    Updates Driver routes with previousbutton

--- src/pages/private/Driver/CarRoutes.js
+++ src/pages/private/Driver/CarRoutes.js
@@ -1,2 +1,9 @@
-import React from 'react'
-export default () => <h1>Car routes</h1>
+import React, { Fragment } from 'react'
+import PreviousButton from '../../../components/PreviousButton'
+
+export default () => (
+	<Fragment>
+		<PreviousButton />
+		<h1>Car routes</h1>
+	</Fragment>
+)
