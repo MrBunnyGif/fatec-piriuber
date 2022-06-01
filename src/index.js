ccc924f8e4c692b430beabad3d59c26a61bd8cec
Note Atelie <pedro.botega@ateliedepropaganda.com.br>
2022-06-01 12:48:36 -0300

    Creates blank pages

--- src/index.js
+++ src/index.js
@@ -7,11 +7,13 @@ import {
   Routes,
   Route,
 } from "react-router-dom";
+import LoginType from './pages/public/Logintype'
 
 ReactDOM.render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />} />
+      <Route path="/login-type" element={<LoginType />} />
     </Routes>
   </BrowserRouter>,
   document.getElementById('root')
---
+++ src/pages/private/Parents/DriverContact.js
@@ -0,0 +1,2 @@
+import React from 'react'
+export default () => <h1>Driver contact</h1>
