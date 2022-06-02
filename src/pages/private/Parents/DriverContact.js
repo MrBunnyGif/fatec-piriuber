2b904aed7cb9cf667164f0c714dc949ce992cfe2
Note Atelie <pedro.botega@ateliedepropaganda.com.br>
2022-06-02 19:34:17 -0300

    Configures Drivercontact links

--- src/pages/private/Parents/DriverContact.js
+++ src/pages/private/Parents/DriverContact.js
@@ -1,16 +1,18 @@
 import React, { Fragment } from 'react'
 import PreviousButton from '../../../components/PreviousButton'
+import image from '../../../assets/images/image1.jpeg'
 
 export default () => {
 	return (
 		<Fragment>
 			<PreviousButton />
 			<h1>Driver contact</h1>
-			<img src={''}/>
+			<img width={100} src={image} />
+			<br />
 			<a href="tel:11987654321">
 				whatsapp
 			</a>
-<br/>
+			<br />
 			<a href="mailto:email@test.com.br">
 				email
 			</a>
