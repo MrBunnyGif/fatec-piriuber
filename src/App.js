e5a5d3c4ef73d43bd4eb9e9d71fcea78b2d53d9a
Note Atelie <pedro.botega@ateliedepropaganda.com.br>
2022-06-01 22:29:09 -0300

    Moves Routes to App

--- src/App.js
+++ src/App.js
@@ -1,19 +1,36 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
- 
+import LoginType from './pages/public/Logintype'
+import DriverContact from './pages/private/Parents/DriverContact'
+import DriversList from './pages/private/Parents/DriversList'
+import FindChildren from './pages/private/Parents/FindChildren'
+import ParentsProfile from './pages/private/Parents/Profile'
+import CarRoutes from './pages/private/Driver/CarRoutes'
+import Checkin from './pages/private/Driver/Checkin'
+import Children from './pages/private/Driver/Children'
+import ParentsContact from './pages/private/Driver/ParentsContact'
+import Home from './pages/private/Home'
+import {
+  Routes,
+  Route,
+} from "react-router-dom";
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Routes>
+        <Route path="/" element={<LoginType />} />
+        <Route path="/home" element={<Home />} />
+
+        <Route path="/find-children" element={<FindChildren />} />
+        <Route path="/driver-contact" element={<DriverContact />} />
+        <Route path="/drivers-list" element={<DriversList />} />
+        <Route path="/parents-profile" element={<ParentsProfile />} />
+
+        <Route path="/checkin" element={<Checkin />} />
+        <Route path="/car-routes" element={<CarRoutes />} />
+        <Route path="/parents-contact" element={<ParentsContact />} />
+        <Route path="/manage-children" element={<Children />} />
+      </Routes>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -2,38 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
-import LoginType from './pages/public/Logintype'
-import DriverContact from './pages/private/Parents/DriverContact'
-import DriversList from './pages/private/Parents/DriversList'
-import FindChildren from './pages/private/Parents/FindChildren'
-import ParentsProfile from './pages/private/Parents/Profile'
-import CarRoutes from './pages/private/Driver/CarRoutes'
-import Checkin from './pages/private/Driver/Checkin'
-import Children from './pages/private/Driver/Children'
-import ParentsContact from './pages/private/Driver/ParentsContact'
-import Home from './pages/private/Home'
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<LoginType />} />
-      <Route path="/home" element={<Home />} />
-
-      <Route path="/find-children" element={<FindChildren />} />
-      <Route path="/driver-contact" element={<DriverContact />} />
-      <Route path="/drivers-list" element={<DriversList />} />
-      <Route path="/parents-profile" element={<ParentsProfile />} />
-
-      <Route path="/checkin" element={<Checkin />} />
-      <Route path="/car-routes" element={<CarRoutes />} />
-      <Route path="/parents-contact" element={<ParentsContact />} />
-      <Route path="/manage-children" element={<Children />} />
-    </Routes>
+    <App />
   </BrowserRouter>,
   document.getElementById('root')
 );
