5421a55e4275728d145956dfe5c3d87cbf5683bf
Note Atelie <pedro.botega@ateliedepropaganda.com.br>
2022-06-08 14:53:09 -0300

    Contrato page

--- src/pages/private/Parents/Profile.js
+++ src/pages/private/Parents/Profile.js
@@ -1,16 +1,36 @@
-import React, { Fragment } from 'react'
+import React, { Fragment as div } from 'react'
 import PreviousButton from '../../../components/PreviousButton'
 
 export default () => {
 	return (
-		<Fragment>
-			<PreviousButton />
-			<p>
-				Apenas dados cadastrais, nenhuma funcionalidade.
-			</p>
-			<p>
-				Pegar tela com o caue
-			</p>
-		</Fragment>
+		<div className='contrato'>
+			<div class="toggle">
+				<i class="fa fa-bars menu-icone" aria-hidden="true" style={{ transform: "translateX(-30%)" }}></i>
+			</div>
+
+			<header>
+				<PreviousButton />
+				<h1>Contrato</h1>
+			</header>
+
+
+			<div id="div-fundo-texto">
+				<div id="div-texto">
+					{/* <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p> */}
+					<p>
+						Anim nisi voluptate enim dolor ipsum dolore deserunt excepteur voluptate irure laboris id do eu. Aliquip eiusmod esse incididunt consequat anim ipsum eu do id exercitation ut deserunt. Lorem ex consequat esse mollit veniam ullamco magna. Consequat occaecat amet elit eiusmod enim. Aliquip laborum Lorem in occaecat exercitation eu adipisicing. Cupidatat irure est exercitation est reprehenderit.
+						<br />
+						<br />
+						Pariatur commodo amet enim ex eiusmod. Labore ea laborum excepteur laborum duis in voluptate amet Lorem voluptate ullamco nisi fugiat reprehenderit. Sint nisi aliquip cupidatat et irure sint. Pariatur sint consequat Lorem est occaecat dolor amet velit Lorem culpa quis. Ea velit irure adipisicing culpa commodo nostrud. Magna irure sit minim dolore id aliquip nostrud nulla excepteur. Consequat cupidatat exercitation ea nisi do veniam esse incididunt minim nulla do.
+						<br />
+						Tempor nulla aliquip magna sunt excepteur nulla officia dolor. Esse minim aliquip ea cillum ea consectetur minim. Tempor officia aliqua ut officia anim labore et quis. Dolore in quis dolor et consequat ex officia id do anim ex. Excepteur commodo mollit amet ex enim aliquip in. Proident exercitation laborum dolor esse.
+						<br />
+						<br />
+						Mollit deserunt magna adipisicing aliquip magna mollit est nisi id nisi. Irure reprehenderit quis laborum est. Aliqua esse fugiat officia tempor mollit est quis enim aliqua veniam. Labore voluptate adipisicing eu occaecat commodo id consequat irure ipsum aliqua dolor ullamco anim enim. Adipisicing voluptate cillum laborum sit proident voluptate. Laborum anim voluptate Lorem nostrud eiusmod magna minim occaecat proident quis tempor culpa. Tempor cupidatat qui sit velit commodo irure non culpa in deserunt ex cupidatat exercitation pariatur.
+					</p>
+
+				</div>
+			</div>
+		</div>
 	)
 }
